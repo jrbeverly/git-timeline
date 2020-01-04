af7acefcae9a27409a3393153c03589a85f6c00e
jrbeverly <jrbeverly@live.com>
2020-01-03 21:03:36 -0500

    History without any time

--- git-timeline.bash
+++ git-timeline.bash
@@ -24,6 +24,11 @@ history () {
     git log --oneline --pretty=format:"%h %ad" > ../HISTORY
 }
 
+history-no-time () {
+    cd demo/
+    git log --oneline --pretty=format:"%h " > ../HISTORY
+}
+
 git-rebase-custom() {
     GIT_SEQUENCE_EDITOR="sed -i -ze 's/^pick/e/'" git rebase "$1^" -i
     GIT_COMMITTER_DATE="$2" git commit --amend --no-edit --date "$2"
